0d6bab2f59630efa6b25bc8f3270e8844573095a
Yuelin Ding <d29171314@gmail.com>
2026-03-18 09:45:30 +0800

    chore: 优化注释表达

--- AcWing/1-基础算法/3722_骑车路线.cpp
+++ AcWing/1-基础算法/3722_骑车路线.cpp
@@ -1,7 +1,8 @@
 /*
 https://www.acwing.com/problem/content/3725/
-
 双指针-跳跃指针
+
+如何写得更优雅—>双层while循环
 */
 
 #include<iostream>
@@ -21,13 +22,14 @@ int main()
 		//跳跃双指针
 		int res = 0;
 		int l = 0;
-		while(l < n) //区间的左端点
+		while(l < n) //维护[l,r]，使其为一个爬坡
 		{
 			int r = l;
 			while(r+1<n && p[r+1]>=p[r]) //注意这里的r+1<n是为了防止越界
 				r ++;
-			//while循环退出之后就是最大的非下降区间了
-			if(r > l)
+            //while退出有两种可能：r==n-1 or p[r+1]<p[r]
+            //这两种可能均表示[l,r]已到达极限
+			if(r > l) //满足区间长度>=2的要求
 				res = max(res, p[r]-p[l]);
 			//因为p[r+1]<p[r]，所以起点l直接设为r+1即可
 			l = r+1;
