a3e931449d327ed10ba1f7a7b0c000da08b8d158
Yuelin Ding <d29171314@gmail.com>
2026-04-13 17:03:49 +0800

    2026蓝桥杯C++B组：A题

---
+++ LuoGu/第十七届蓝桥杯省赛CppB组/A.cpp
@@ -0,0 +1,16 @@
+/*
+https://www.luogu.com.cn/problem/P16232
+补题-蓝桥杯2026 C++B组 A题
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	long long n = 2026202520242023;
+	cout << n/2+1 << endl;
+	return 0;
+}
