b077ad25f844800880d2fbe51a0d865889e157b3
Yuelin Ding <d29171314@gmail.com>
2026-04-13 17:04:03 +0800

    2026蓝桥杯C++B组：C题

---
+++ LuoGu/第十七届蓝桥杯省赛CppB组/C.cpp
@@ -0,0 +1,29 @@
+/*
+https://www.luogu.com.cn/problem/P16234
+补题-蓝桥杯2026 C++B组 C题
+*/
+#include<iostream>
+using namespace std;
+
+int t;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	cin >> t;
+	while(t --)
+	{
+		long long n,x,y;
+		cin >> n >> x >> y;
+		
+		//也就是[x,y]之间的数的个数
+		if(y-x >= 0)
+			cout << y-x+1 << "\n";
+		else
+			cout << 0 << "\n";
+	}
+	return 0;
+	
+}
