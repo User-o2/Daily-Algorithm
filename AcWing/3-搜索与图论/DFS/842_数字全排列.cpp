75c7f388c7baa9dc70844518cdcedc34790d4831
Yuelin Ding <d29171314@gmail.com>
2026-05-04 22:44:09 +0800

    DFS：N叉树搜索模型-数字全排列

--- AcWing/3-搜索与图论/DFS/842_数字全排列.cpp
+++ AcWing/3-搜索与图论/DFS/842_数字全排列.cpp
@@ -1,45 +1,48 @@
 /*
 https://www.acwing.com/problem/content/844/
-dfs全排列
+N叉树搜索模型
 */
 #include<iostream>
+#include<vector>
 using namespace std;
 
-const int N = 15;
-int n,path[N]; //存储某一分支（一种全排列顺序）
-bool sta[N]; //是否已经被使用
+const int N = 10;
+int n;
+int path[N]; //路径是定长的，使用静态数组即可
+bool vis[N]; //是否访问
 
-void dfs(int u) //u表示要填的坑位
+void dfs(int u) //u表示该填的坑位，坑位1~n
 {
-	if(u == n+1) //要填坑位n+1了，结束
-	{
-		for(int i = 1; i <= n; i ++) //输出坑位1-n
-			cout << path[i] << " ";
-		cout << "\n";
-		return;
-	}
-	
-	//对于本坑位，尝试所有潜在的数字
-	for(int i = 1; i <= n; i ++) //尝试数字1~n
-	{
-		if(sta[i] == false)
-		{
-			sta[i] = true;
-			path[u] = i; //在坑位u填上数字i
-			dfs(u+1); //去填下一个坑位
-			//恢复现场
-			path[u] = 0;
-			sta[i] = false;
-		}
-	}
+    if(u == n+1)
+    {
+        for(int i = 1; i <= n; i ++)
+            cout << path[i] << " ";
+        cout << "\n";
+        return;
+    }
+    
+    //对于本坑位，所有没有使用过的数字都是可能的
+    for(int i = 1; i <= n; i ++) //数字1~n
+    {
+        if(vis[i] == false) //还没有用过
+        {
+            vis[i] = true;
+            path[u] = i; //填上坑位u
+            dfs(u+1);
+            //恢复现场
+            vis[i] = false;
+            path[u] = 0; //可不做
+        }
+    }
 }
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	
-	cin >> n;
-	dfs(1); //从坑位1开始填，需要填1~n
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> n;
+    //统一基于1的索引
+    dfs(1);
+    return 0;
 }
