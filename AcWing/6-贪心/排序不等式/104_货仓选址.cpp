879233a98a3fc82c374247b11761e277283f80d1
Yuelin Ding <d29171314@gmail.com>
2026-07-12 20:48:29 +0800

    贪心：绝对值三角不等式

---
+++ AcWing/6-贪心/排序不等式/104_货仓选址.cpp
@@ -0,0 +1,33 @@
+/*
+https://www.acwing.com/problem/content/106/
+货仓选址
+
+【绝对值三角不等式】|x|+|y|>=|x-y|
+*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+const int N = 100010;
+int a[N];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int n;
+	cin >> n;
+	for(int i = 0; i < n; i ++)
+		cin >> a[i];
+	
+	sort(a,a+n);
+	
+	int mid = a[n/2];
+	long long res = 0;
+	for(int i = 0; i < n; i ++)
+		res += abs(mid-a[i]);
+		
+	cout << res << "\n";
+	return 0;
+}
